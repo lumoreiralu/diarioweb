8d7891d540d54c77abc0a1b36219bc7b03f6c40a
lumoreira <lulii.moreira96@gmail.com>
2024-05-03 11:51:44 -0300

    avanzojs

---
+++ js/js.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function saludar(){
+    let saludo = "Estas entrando al diario del Caniche";
+    alert(saludo);
+}
+saludar();
+
+let divparajava= document.querySelector("#divjava");
+divparajava.innerHTML= "soy lo primero que modifico con js"
